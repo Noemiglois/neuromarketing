d05a839b29548d110fee844dfeb2b44261d58e22
Noemi Gonzalez <noemigonzalezlois@gmail.com>
2020-02-13 13:51:07 +0100

    Análisis inicial estructura de datos

--- TFG_utils.py
+++ TFG_utils.py
@@ -3,57 +3,59 @@
 
 import os
 
+# Se escribe la info del objeto en el fichero Data_Info_Eeg (abrir, escribir, cerrar)
 def Load_Eeg_Info(Eeg_Info):
 	Info_File = open('Data_Info_Eeg','w')
 	Info_File.write(str(Eeg_Info))
 	Info_File.close()
-
 	return 'Data_Info_Eeg'
 
+# Se busca la frecuencia de muestreo en el fichero Data_Info_Eeg (abrir, leer,buscar)
 def Get_Frequency(Info_File):
 	Info_File = open('Data_Info_Eeg', 'r')
 	Info = Info_File.read()
 	Freq_Line = Info.splitlines()[16]
 	Freq = Freq_Line.split('|')[1].split(' ')[1].split('.')[0]
+    #¿CERRAR FICHERO?
 	return Freq
 
-def take_vmrk_file(Eeg_path):
-	dirs = os.listdir(os.getcwd() + "\\" + Eeg_path)
-	dir_len = len(dirs)
-	i = 0
-	while i < dir_len:
-		x = dirs[i].split('.')
-		if x[1] == 'vmrk':
-			return dirs[i]
-		else:
-			i += 1
+# Se saca el nombre del fichero vmrk
+def take_vmrk_filename(Eeg_path):
+    for file in os.listdir(Eeg_path):
+        if file.split(".")[1]== "vmrk":
+            vmrk_filename = os.path.join(Eeg_path, file)
+    return vmrk_filename
 
-def Get_Start_End(File):
+# Se saca el nombre del fichero vhdr
+def take_vhdr_filename(Eeg_path):
+    for file in os.listdir(Eeg_path):
+        if file.split(".")[1]== "vhdr":
+            vhdr_filename = os.path.join(Eeg_path, file)
+    return vhdr_filename
+
+# Se busca el inicio de los anuncios en el registro de EEG
+def Get_Start(File):
     
 	Info_File = open(File, 'r')
 	Info = Info_File.read()
+
 	Info_Lines = Info.splitlines()
-
 	Total_Lines  = len(Info_Lines)
-
-	Data_Start_End = []
-
+    
 	i = 0
 	while i<Total_Lines:
 		Line_Data = Info_Lines[i]
-		if Line_Data[:3] == 'Mk2' or Line_Data[:3] == 'Mk3':
-			Data = Line_Data.split(',')
-			start_end = Data[2]
-			Data_Start_End.append(start_end)
+		if Line_Data[:3] == 'Mk2':
+			start = Line_Data.split(',')[2]
 			i += 1
 		else:
 			i += 1
-	return Data_Start_End
+	return start
 
-def spot_samples(start_end, freq, spot_secs):
+# Se sacan los valores de EEG en cada comienzo de anuncio
+def spot_samples(start, freq, spot_secs):
 	spot_samples = []
-	print("before")
 	for i in spot_secs:
-		sample = int(i)*int(freq) + int(start_end[0])
+		sample = int(i)*int(freq) + int(start)
 		spot_samples.append(sample)
 	return spot_samples
